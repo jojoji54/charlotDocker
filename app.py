093688110680aaf90cafae409081e115356a8d31
jojoji54 <35396962+jojoji54@users.noreply.github.com>
2021-06-04 01:29:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,79 +2,48 @@ import random
 import json
 from flask import Flask, jsonify,request
 from flask_cors import CORS
-import time
-
 import torch
-
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
+import time
+import os
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-with open('intents.json', 'r') as json_data:
-    intents = json.load(json_data)
-
-FILE = "data.pth"
-data = torch.load(FILE)
-
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+app = Flask(__name__, static_url_path='');
+os.system('python database.py')
+os.system('python trainMessages.py')
 
 
 
-""" app = Flask(__name__);
+CORS(app)
+@app.route("/", methods=["GET"])
+def home():
+    return '<h1>Demo</h1>'
 CORS(app)
 @app.route("/bot", methods=["POST"])
-
-#response
 def response():
-   
-   # res = query + " " 
-   query = dict(request.form)['query']
-   res = query + ""
-   res = tokenize(res)
-    
-    X = bag_of_words(res, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+    app.logger.info('start')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    app.logger.info(device)
+    with open('UserMessages.json', 'r') as json_data:
+        intents = json.load(json_data)
+    app.logger.info(intents)
+    FILE = "data2.pth"
+    data = torch.load(FILE)
+    app.logger.info(data)
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-
-    tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-               
-
-    # print(f"{bot_name}: {random.choice(intent['responses'])}")
-    return jsonify({"response" :  random.choice(intent['responses'])})
-
-    
-     
-if __name__=="__main__":
-    app.run(host="0.0.0.0",) """
-    
-    
-app = Flask(__name__);
-CORS(app)
-@app.route("/bot", methods=["POST"])
-
-#response
-def response():
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+    # return '<h2>sdfjk</h2>'
     query = dict(request.form)['query']
+    
+
     res = query 
     res =  tokenize(res)
     
@@ -88,45 +57,12 @@ def response():
     
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                
-                return jsonify({"response" : random.choice(intent['responses'])})
-    else:
-        return jsonify({"response" : " I do not understand..."})
-       
-     
-if __name__=="__main__":
-    app.run(host="0.0.0.0",) 
     
-
-
-
-
-""" bot_name = "Charlot"
-print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
-
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
-
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-
-    tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
+        app.logger.info('%d logged in successfully', prob.item())
+        app.logger.info(intents['intents'])
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")  """
+                return jsonify({"response" : random.choice(intent['responses'])})
+        
+    
